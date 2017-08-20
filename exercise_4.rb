eb6058f8505695b883609a797866a44844aba4db
robincollins <racollins1@me.com>
2017-08-20 19:13:32 +0100

    ex 4 update

--- exercise_4.rb
+++ exercise_4.rb
@@ -1,11 +1,46 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit the return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  if name.empty?
+    puts nil
+  end
+
+
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------"
+end
+
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  x = 0
+  while x < students.length
+    puts "#{students[x][:name]} (#{students[x][:cohort]} cohort)"
+    x += 1
   end
 end
 
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
 
-
-  until 
-
-  end
+students = input_students
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
