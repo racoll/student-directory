9729741f23341723990de47a0ea203e21f00c205
robincollins <racollins1@me.com>
2017-08-21 10:15:57 +0100

    csv update

--- interactive_menu.rb
+++ interactive_menu.rb
@@ -26,6 +26,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -41,6 +42,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -53,14 +56,27 @@ def print_header
   puts "--------------"
 end
 
-def print(students)
+def print_student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
+def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
+
+def save_students
+  # open file for viewing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 
 interactive_menu
