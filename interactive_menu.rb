f74c4eb9a8d1f67f572bace5790466fbaf604fdf
robincollins <racollins1@me.com>
2017-08-21 09:31:01 +0100

    int menu update

--- interactive_menu.rb
+++ interactive_menu.rb
@@ -1,3 +1,6 @@
+@students = [] # an empty array accessible to all methods
+
+
 def interactive_menu
   students = []
   loop do
@@ -20,5 +23,38 @@ def interactive_menu
     else
       puts "I don't know what you meant, try again"
     end
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print(students)
+  print_footer(students)
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
   end
 end
